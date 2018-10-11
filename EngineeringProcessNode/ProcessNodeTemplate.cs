5ba0650788957368e2d1688a00dd3bd2e0a83c45
Chanple <chanple@outlook.com>
2018-10-11 23:58:57 +0800

    ~

---
+++ EngineeringProcessNode/ProcessNodeTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineeringProcessNode
+{
+    /// <summary>
+    /// 工程进度模板
+    /// </summary>
+    public class ProcessNodeTemplate
+    {
+
+    }
+}
---
+++ EngineeringProcessNodeTests/ProcessNodeHelperTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EngineeringProcessNode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineeringProcessNode.Tests
+{
+    [TestClass()]
+    public class ProcessNodeHelperTests
+    {
+        [TestMethod()]
+        public void IncreaseDaysbyPbtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IncreaseHoursbyPbtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReduceDaysbyPctTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReduceHoursbyPctTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetStatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddStatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddStatusTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddBranchPreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddBranchNexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteStatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteStatusTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteBranchPreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteBranchNexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasStatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasStatusTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasStatusTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasBranchPreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasBranchNexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
